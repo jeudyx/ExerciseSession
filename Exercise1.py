956686a0a9edc65d6f07117798a5baaf29ddc51b
Jeudy Blanco <jeudyx@gmail.com>
2012-02-21 18:13:34 +0100

    Got containsNResponse function and with a small test

--- Exercise1.py
+++ Exercise1.py
@@ -11,13 +11,26 @@ def main(argv):
 	
 	path = argv[1]
 
-	print "Hola. El path es: " + path
-	for f in getFileNames(path) :
-		print "File: " + f
+	print containsNResponse("CI type: 8")
+	print containsNResponse("Sex: N")
 
+	#print "Hola. El path es: " + path
+	#for f in getFileNames(path) :
+	#	print "File: " + f
+
+#This function returns the content of a directory in a list
 def getFileNames(path):
 	return os.listdir(path)
 
+'''
+This function returns true if the provided line contains a 
+negative response. Assumes upper case N separated 
+by : and a white space after the question
+'''
+def containsNResponse(line):
+	return line.find(": N") > 0
 #---------------------------
+
+
 
 main(sys.argv)
