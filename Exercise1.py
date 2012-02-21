829f6f6dea7b637e7bb0c354a114501b020ea72a
Jeudy Blanco <jeudyx@gmail.com>
2012-02-21 18:57:07 +0100

    Done

--- Exercise1.py
+++ Exercise1.py
@@ -6,6 +6,7 @@ Jeudy Blanco - 02/21/2012
 import sys
 import os
 
+
 #This will be the program's entry point
 def main(argv):
 	
@@ -34,11 +35,30 @@ def containsNResponse(line):
 Path must end in /
 '''
 def fixDataFile(path, filename):
+	tempfile = open(path + filename + ".tmp", 'w')	
 	filehandle = open(path + filename, 'r')
+
+	rename_needed = False
+
 	for line in filehandle:
 		if containsNResponse(line):
 			correct_line = line.replace(": N", ": M")
 			print "Negative response found in file: " + path + filename + " -- response: " + line + " -- corrected: " + correct_line
+			tempfile.write(correct_line)
+			rename_needed = True
+		else:
+			tempfile.write(line)
+	
 	filehandle.close()
+	tempfile.close()
+#Need to rename the new file, and take a backup of the old one
+	
+	if rename_needed :
+		os.rename(path + filename, path + filename + ".old")
+		os.rename(path + filename + ".tmp", path + filename)
+	else:
+		os.remove(path + filename + ".tmp")
+
+#----------------------------
 
 main(sys.argv)
