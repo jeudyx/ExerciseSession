df6f682545aeef3ca5e93d503567e147df78890e
Jeudy Blanco <jeudyx@gmail.com>
2012-02-22 17:27:01 +0100

    Extracted the unique papers list

--- Exercise2.py
+++ Exercise2.py
@@ -14,18 +14,22 @@ class RecommendationsHolder(object):
 	def __init__(self, inputdatadicc):
 		self.Persons = inputdatadicc.keys()
 		self.Papers = set()			#Initially, the set of paper names is empty, will add them one by one
+		for entry in inputdatadicc.values():
+			for paper_name in entry.keys():
+				self.Papers.add(paper_name)
 
 	def PrintPersons(self):
 		print self.Persons
 
+	def PrintPapers(self):
+		print self.Papers
 
 #--------------------
 def main(data):
 	holder = RecommendationsHolder(data)
-	#holder.PrintPersons()
+	holder.PrintPapers()
 	#print '-----------------'
 	#print holder.Persons[2]	
 	
-	print data.values()[0].keys()
 
 main(inputdata.raw_scores)
