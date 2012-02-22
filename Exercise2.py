d8963aca74c4b8922c800c17f12ccfb9d663d0ab
Jeudy Blanco <jeudyx@gmail.com>
2012-02-22 17:23:13 +0100

    Extracted the persons list

--- Exercise2.py
+++ Exercise2.py
@@ -6,6 +6,26 @@ Jeudy Blanco - 02/22/2012
 import sys
 import os
 import numpy
+import inputdata
 
 
+class RecommendationsHolder(object):
+	#Class constructor	
+	def __init__(self, inputdatadicc):
+		self.Persons = inputdatadicc.keys()
+		self.Papers = set()			#Initially, the set of paper names is empty, will add them one by one
 
+	def PrintPersons(self):
+		print self.Persons
+
+
+#--------------------
+def main(data):
+	holder = RecommendationsHolder(data)
+	#holder.PrintPersons()
+	#print '-----------------'
+	#print holder.Persons[2]	
+	
+	print data.values()[0].keys()
+
+main(inputdata.raw_scores)
