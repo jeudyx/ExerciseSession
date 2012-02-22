52db5d901425c0817fc6a92f558783062e446883
Jeudy Blanco <jeudyx@gmail.com>
2012-02-22 17:55:00 +0100

    Part 1 done, I am a genious xD

--- Exercise2.py
+++ Exercise2.py
@@ -8,28 +8,43 @@ import os
 import numpy
 import inputdata
 
-
+#Part 1: Create a NumPy array with the recommendations
 class RecommendationsHolder(object):
 	#Class constructor	
 	def __init__(self, inputdatadicc):
 		self.Persons = inputdatadicc.keys()
 		self.Papers = set()			#Initially, the set of paper names is empty, will add them one by one
+
+		#Adds the paper names into a set to ensure they are unique		
 		for entry in inputdatadicc.values():
 			for paper_name in entry.keys():
 				self.Papers.add(paper_name)
 
-	def PrintPersons(self):
-		print self.Persons
+		#Now that I have all the persons and the papers, I know how big the numpy array as to be
 
-	def PrintPapers(self):
-		print self.Papers
-
+		self.Ratings = numpy.ndarray( (len(self.Persons), len(self.Papers)), dtype=float) 
+		
+		self.Ratings[:] = 0.
+		
+		#Now I can store the ratings
+		i = 0		
+		while(i < len(self.Persons)):
+			j = 0			
+			while(j < len(self.Papers)):
+				
+				if inputdatadicc[self.Persons[i]].has_key(list(self.Papers)[j]):
+					 self.Ratings[i,j] = inputdatadicc[self.Persons[i]][list(self.Papers)[j]]
+				else:
+					self.Ratings[i,j] = 0.
+				j = j + 1
+			
+			i = i + 1
 #--------------------
 def main(data):
 	holder = RecommendationsHolder(data)
-	holder.PrintPapers()
+	#holder.PrintPapers()
 	#print '-----------------'
 	#print holder.Persons[2]	
-	
+	print holder.Ratings
 
 main(inputdata.raw_scores)
